4bfc07952021ac01c27d99417975b9f8020fe318
Eduardo Flores <toomanyeduardos@gmail.com>
2016-06-14 20:25:33 -0600

    Created json parser to get the href of image-thumbnails for memories

--- FamilySearchIosSampleApp/MemoriesVC.swift
+++ FamilySearchIosSampleApp/MemoriesVC.swift
@@ -14,6 +14,50 @@ class MemoriesVC : UIViewController
     var user : User!
     
     override func viewDidLoad() {
-        print("memories url = \(user.artifactsHref!)")
+        
+        // get the access token from NSUserDefaults
+        let preferences = NSUserDefaults.standardUserDefaults()
+        let accessToken = preferences.stringForKey(Utilities.KEY_ACCESS_TOKEN)
+        
+        getMemoriesImagesForUser(accessToken!)
+    }
+    
+    func getMemoriesImagesForUser(accessToken:String) -> ()
+    {
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration();
+        let headers: [NSObject : AnyObject] = ["Accept":"application/json", "Authorization":"Bearer " + accessToken];
+        configuration.HTTPAdditionalHeaders = headers;
+        let session = NSURLSession(configuration: configuration)
+        
+        let memoriesTask = session.dataTaskWithURL(NSURL(string:user.artifactsHref!)! ) { (memoriesData, response, memoriesError) in
+            do
+            {
+                let memoriesDataJson = try NSJSONSerialization.JSONObjectWithData(memoriesData!, options: .AllowFragments);
+                
+                let sourceDescriptions = memoriesDataJson["sourceDescriptions"] as? [NSDictionary]
+                for sourceDescription in sourceDescriptions!
+                {
+                    // for this demo we're only downloading images, so we need to check if the sourceDescription contains an image
+                    let mediaType = sourceDescription["mediaType"] as? String
+                    if (mediaType == "image/jpeg")
+                    {
+                        let links = sourceDescription["links"] as? NSDictionary
+                        let linkImageThumbnail = links!["image-thumbnail"] as? NSDictionary
+                        let linkImageThumbnailHref = linkImageThumbnail!["href"] as? String
+                        print("linkImageThumbnailHref = \(linkImageThumbnailHref!)")
+                    }
+                    else
+                    {
+                        continue
+                    }
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+        memoriesTask.resume()
+
     }
 }
